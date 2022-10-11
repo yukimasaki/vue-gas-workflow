8e6838a0350cdba8b5024d6bc2c5f6d68b853cc1
yukimasaki <woodenhoe@gmail.com>
2022-10-11 06:25:07 +0000

    update: Home.vueとSettings.vueへのルーティングを追加

--- src/vue-gas-workflow/src/router/index.js
+++ src/vue-gas-workflow/src/router/index.js
@@ -1,28 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import Settings from '../views/Settings.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/settings',
+    name: 'Settings',
+    component: Settings
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
