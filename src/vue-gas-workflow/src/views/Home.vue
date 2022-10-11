d04b9a945fffe090b7f93068654617614af719eb
yukimasaki <woodenhoe@gmail.com>
2022-10-11 06:55:31 +0000

    update: Home.vue 申請書をカード形式で表示

--- src/vue-gas-workflow/src/views/Home.vue
+++ src/vue-gas-workflow/src/views/Home.vue
@@ -1,11 +1,31 @@
 <template>
-    <div>
-      <h1>Home だよ</h1>
-    </div>
+    <v-row justify="center">
+      <v-col cols="12" sm="12" md="4">
+        <v-card
+          class="mb-2"
+          v-for="form in forms"
+            :key="form">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ form.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
   </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+
+  data() {
+    return {
+      forms: [
+        {name: '休暇申請', link: '/paid_leave'},
+        {name: '備品申請', link: '/equipment'},
+      ],
+    }
+  }
 }
 </script>
