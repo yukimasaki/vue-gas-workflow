dfface07b0473f18ba37b1270b38720d03336026
yukimasaki <woodenhoe@gmail.com>
2022-10-11 08:30:57 +0000

    update: PaidLeave.vue ItemDialog.vueをインポート

--- src/vue-gas-workflow/src/views/PaidLeave.vue
+++ src/vue-gas-workflow/src/views/PaidLeave.vue
@@ -1,52 +1,63 @@
 <template>
-  <v-card>
-    <v-card-title>
-      <!-- 申請書タイトル -->
-      <v-col cols="8">
-        <div class="text-h3">
-          {{ title }}
-        </div>
-      </v-col>
-      <v-spacer/>
-      <!-- 追加ボタン -->
-      <v-col class="text-right" cols="4">
-        <v-btn dark color="green">
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </v-col>
-      <!-- 検索フォーム -->
-      <v-col cols="12">
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-        />
-      </v-col>
-    </v-card-title>
+  <div>
+    <v-card>
+      <v-card-title>
+        <!-- 申請書タイトル -->
+        <v-col cols="8">
+          <div class="text-h3">
+            {{ title }}
+          </div>
+        </v-col>
+        <v-spacer/>
+        <!-- 追加ボタン -->
+        <v-col class="text-right" cols="4">
+          <v-btn dark color="green" @click="onClickAdd">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </v-col>
+        <!-- 検索フォーム -->
+        <v-col cols="12">
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          />
+        </v-col>
+      </v-card-title>
+      <!-- テーブル -->
+      <v-data-table
+        class="text-no-wrap"
+        :headers="tableHeaders"
+        :items="tableData"
+        :search="search"
+        :footer-props="footerProps"
+        :loading="loading"
+        :sort-by="'id'"
+        :sort-desc="true"
+        :items-per-page="30"
+        mobile-breakpoint="0"
+      >
+      </v-data-table>
+    </v-card>
 
-    <!-- テーブル -->
-    <v-data-table
-      class="text-no-wrap"
-      :headers="tableHeaders"
-      :items="tableData"
-      :search="search"
-      :footer-props="footerProps"
-      :loading="loading"
-      :sort-by="'id'"
-      :sort-desc="true"
-      :items-per-page="30"
-      mobile-breakpoint="0"
-    >
-    </v-data-table>
+    <!-- 追加／編集ダイアログ -->
+    <ItemDialog ref="itemDialog"/>
 
-  </v-card>
+  </div>
+
 </template>
 
 <script>
+import ItemDialog from '../components/ItemDialog.vue'
+
 export default {
   name: 'PaidLeave',
+
+  components: {
+    ItemDialog
+  },
 
   data() {
     return {
@@ -87,5 +98,12 @@ export default {
       return { itemsPerPageText: '', itemsPerPageOptions: [] }
     }
   },
+
+  methods: {
+    /** 追加ボタンがクリックされたとき */
+    onClickAdd () {
+      this.$refs.itemDialog.open('add')
+    },
+  }
 }
 </script>
