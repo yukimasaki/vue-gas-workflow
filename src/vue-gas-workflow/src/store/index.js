50c70436c54a01ca5943a5df8526ad61f9545c71
yukimasaki <woodenhoe@gmail.com>
2022-10-12 02:00:06 +0000

    update: store/index.js 家計簿データのミューテーション、アクションを実装

--- src/vue-gas-workflow/src/store/index.js
+++ src/vue-gas-workflow/src/store/index.js
@@ -16,7 +16,10 @@ const state = {
     strIncomeItems: '給料, ボーナス, 繰越',
     strOutgoItems: '食費, 趣味, 交通費, 買い物, 交際費, 生活費, 住宅, 通信, 車, 税金',
     strTagItems: '固定費, カード'
-  }
+  },
+
+  /** 家計簿データ */
+  abData: {},
 }
 
 /**
@@ -39,7 +42,40 @@ const mutations = {
       state.settings = Object.assign(state.settings, settings)
     }
     document.title = state.settings.appName
-  }
+  },
+
+  /** 指定年月の家計簿データをセットします */
+  setAbData (state, { yearMonth, list }) {
+    state.abData[yearMonth] = list
+  },
+
+  /** データを追加します */
+  addAbData (state, { item }) {
+    const yearMonth = item.date.slice(0, 7)
+    const list = state.abData[yearMonth]
+    if (list) {
+      list.push(item)
+    }
+  },
+
+  /** 指定年月のデータを更新します */
+  updateAbData (state, { yearMonth, item }) {
+    const list = state.abData[yearMonth]
+    if (list) {
+      const index = list.findIndex(v => v.id === item.id)
+      list.splice(index, 1, item)
+    }
+  },
+
+  /** 指定年月&IDのデータを削除します */
+  deleteAbData (state, { yearMonth, id }) {
+    const list = state.abData[yearMonth]
+    if (list) {
+      const index = list.findIndex(v => v.id === id)
+      list.splice(index, 1)
+    }
+  },
+
 }
 
 /**
@@ -55,7 +91,42 @@ const actions = {
   /** 設定を読み込みます */
   loadSettings ({ commit }) {
     commit('loadSettings')
-  }
+  },
+
+  /** 指定年月の家計簿データを取得します */
+  fetchAbData ({ commit }, { yearMonth }) {
+    // サンプルデータを初期値として入れる
+    const list = [
+      { id: 'a34109ed', date: `${yearMonth}-01`, title: '支出サンプル', category: '買い物', tags: 'タグ1', income: null, outgo: 2000, memo: 'メモ' },
+      { id: '7c8fa764', date: `${yearMonth}-02`, title: '収入サンプル', category: '給料', tags:'タグ1,タグ2', income: 2000, outgo: null, memo: 'メモ' }
+    ]
+    commit('setAbData', { yearMonth, list })
+  },
+
+  /** データを追加します */
+  addAbData ({ commit }, { item }) {
+    commit('addAbData', { item })
+  },
+
+  /** データを更新します */
+  updateAbData ({ commit }, { beforeYM, item }) {
+    const yearMonth = item.date.slice(0, 7)
+    if (yearMonth === beforeYM) {
+      commit('updateAbData', { yearMonth, item })
+      return
+    }
+    const id = item.id
+    commit('deleteAbData', { yearMonth: beforeYM, id })
+    commit('addAbData', { item })
+  },
+
+  /** データを削除します */
+  deleteAbData ({ commit }, { item }) {
+    const yearMonth = item.date.slice(0, 7)
+    const id = item.id
+    commit('deleteAbData', { yearMonth, id })
+  },
+
 }
 
 /** カンマ区切りの文字をトリミングして配列にします */
