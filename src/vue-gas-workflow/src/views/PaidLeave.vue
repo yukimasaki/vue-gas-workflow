3560dd2407dea2fceb4fb7c46cb3115779417238
yukimasaki <woodenhoe@gmail.com>
2022-10-13 00:50:13 +0000

    update: PaidLeave.vue 不要なコメントアウトを削除

--- src/vue-gas-workflow/src/views/PaidLeave.vue
+++ src/vue-gas-workflow/src/views/PaidLeave.vue
@@ -52,7 +52,6 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 import ItemDialog from '../components/ItemDialog.vue'
-// import axios from 'axios'
 
 export default {
   name: 'PaidLeave',
