81448cc3443dfe753b772ff5501715e7ee6fd5dc
yukimasaki <woodenhoe@gmail.com>
2022-10-14 08:33:26 +0000

    todo: firebase.js 文字列型でstateの値を書き換えることには成功した

--- src/vue-gas-workflow/src/store/firebase.js
+++ src/vue-gas-workflow/src/store/firebase.js
@@ -1,8 +1,27 @@
+// 文字列型で値を渡したらstateの書き換えに成功した。
+// オブジェクト型で値を渡す方法について要検証。
+
 const state = {
-  user: { email_address: ''}
+  user: 'test',
+}
+
+const mutations = {
+  /** ユーザー情報をセットする */
+  setUser (state, user) {
+    state.user = user
+  },
+}
+
+const actions = {
+  /** ユーザー情報を保存する */
+  saveUser ({ commit }, user) {
+    commit('setUser', user)
+  },
 }
 
 export default {
   namespaced: true,
-  state
+  state,
+  mutations,
+  actions
 }
