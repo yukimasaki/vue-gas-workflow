9d7c9e5337136016ece12a5b4a974bbfe4e961ca
yukimasaki <woodenhoe@gmail.com>
2022-10-14 03:59:18 +0000

    add: Login.vue作成

---
+++ src/vue-gas-workflow/src/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <button class="btn btn-secondary" v-on:click="authMethod">
+      認証
+    </button>
+    <p class="h5">
+      {{ data.email_address }}
+    </p>
+  </div>
+</template>
+
+<script>
+  import { reactive} from 'vue'
+  import { initializeApp } from 'firebase/app';
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+  export default {
+    props:{
+    },
+    setup(){
+      const authMethod = ()=> {
+
+      const firebaseConfig = {
+        apiKey: "AIzaSyCsCctH0r0gfALLZhQrLC4Y6ywaELOY5pA",
+        authDomain: "vue-auth-e5641.firebaseapp.com",
+        projectId: "vue-auth-e5641",
+        storageBucket: "vue-auth-e5641.appspot.com",
+        messagingSenderId: "37975153108",
+        appId: "1:37975153108:web:c88e0a974a1b751e4d9e41"
+      }
+      initializeApp(firebaseConfig);
+
+      const provider = new GoogleAuthProvider()
+      const auth = getAuth();
+      signInWithPopup(auth, provider).then((result) => {
+        const user = result.user
+        data.email_address = user.email
+      }).catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        const email = error.email
+        console.log('errorCode: ' + errorCode + '/errorMessage: ' + errorMessage + '/email: '+ email)
+      })
+    }
+
+      const data = reactive({
+        email_address: ''
+      })
+      return {
+        data, authMethod
+      }
+    }
+  }
+</script>
