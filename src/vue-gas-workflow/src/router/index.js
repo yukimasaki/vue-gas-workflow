2df76919bb69e13781b3b3e1c37d01d60825261a
yukimasaki <woodenhoe@gmail.com>
2022-10-14 04:02:19 +0000

    update: ログインページへのルーティングを追加

--- src/vue-gas-workflow/src/router/index.js
+++ src/vue-gas-workflow/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Settings from '../views/Settings.vue'
 import PaidLeave from '../views/PaidLeave.vue'
 
@@ -12,6 +13,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/settings',
