ae6fe26b8852603d8292894aa9ad47dd4ae087f4
yukimasaki <woodenhoe@gmail.com>
2022-10-16 09:04:50 +0000

    fix: 不要なimport文を削除

--- src/vue-gas-workflow/src/router/index.js
+++ src/vue-gas-workflow/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
 import Settings from '../views/Settings.vue'
 import PaidLeave from '../views/PaidLeave.vue'
 
