4a74eb0bbe5d0b9755f067ce53ac2fd6a6a84d82
yukimasaki <woodenhoe@gmail.com>
2022-10-16 09:08:31 +0000

    家計簿アプリに関するコードを削除

--- src/vue-gas-workflow/src/views/Settings.vue
+++ src/vue-gas-workflow/src/views/Settings.vue
@@ -24,29 +24,6 @@
         :counter="150"
         :rules="[stringRule]"
       />
-      <h3>カテゴリ／タグ設定</h3>
-      <p>カンマ（ &#44; ）区切りで入力してください。</p>
-      <!-- 収入カテゴリ -->
-      <v-text-field
-        label="収入カテゴリ"
-        v-model="settings.strIncomeItems"
-        :counter="150"
-        :rules="[stringRule, ...categoryRules]"
-      />
-      <!-- 支出カテゴリ -->
-      <v-text-field
-        label="支出カテゴリ"
-        v-model="settings.strOutgoItems"
-        :counter="150"
-        :rules="[stringRule, ...categoryRules]"
-      />
-      <!-- タグ -->
-      <v-text-field
-        label="タグ"
-        v-model="settings.strTagItems"
-        :counter="150"
-        :rules="[stringRule, tagRule]"
-      />
       <v-row class="mt-4">
         <v-spacer/>
         <v-btn color="primary" :disabled="!valid" @click="onClickSave">保存</v-btn>
@@ -61,10 +38,6 @@ export default {
   name: 'Settings',
 
   data () {
-
-    const createItems = v => v.split(',').map(v => v.trim()).filter(v => v.length !== 0)
-    const itemMaxLength = v => createItems(v).reduce((a, c) => Math.max(a, c.length), 0)
-
     return {
 
       /** 設定 */
@@ -81,11 +54,6 @@ export default {
       /** バリデーションルール */
       appNameRule: v => v.length <= 30 || '30文字以内で入力してください',
       stringRule: v => v.length <= 150 || '150文字以内で入力してください',
-      categoryRules: [
-        v => createItems(v).length !== 0 || 'カテゴリは1つ以上必要です',
-        v => itemMaxLength(v) <= 4 || '各カテゴリは4文字以内で入力してください'
-      ],
-      tagRule: v => itemMaxLength(v) <= 4 || '各タグは4文字以内で入力してください'
     }
   },
 
