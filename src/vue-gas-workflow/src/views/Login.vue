560f8fa62c0d70c5bc0610fe5cac85d066eb4255
yukimasaki <woodenhoe@gmail.com>
2022-10-16 05:33:53 +0000

    update: Login.vue ログインボタンを仮設置

--- src/vue-gas-workflow/src/views/Login.vue
+++ src/vue-gas-workflow/src/views/Login.vue
@@ -1,61 +1,36 @@
 <template>
   <div>
-    <button class="btn btn-secondary" @click="authMethod">
+    <button class="btn btn-secondary" @click="loginWithGoogle">
       認証
     </button>
     <p class="h5">
-      <!-- stateの値を取得したい -->
-      {{ user }}
+      <!-- ログインユーザーの表示名を表示する -->
+      {{ userName }}
     </p>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
 import firebaseApp from '../firebase/firebaseConfig'
-import {
-  getAuth, signInWithPopup, GoogleAuthProvider
-} from "firebase/auth";
+import { mapState } from 'vuex'
 
 export default {
   name: 'Login',
 
-  data() {
-    return {
-      data: {}
-    }
-  },
-
   computed: {
     ...mapState({
-      // ↓は正常に動作している
-      user: state => state.firebase.user
+      userName: state => state.firebase.userName
     }),
   },
 
   methods: {
-    ...mapActions(
-      'firebase', ['saveUser']
-    ),
 
-    authMethod() {
+    // Googleアカウントでログイン
+    loginWithGoogle() {
       // 初期化
       firebaseApp
 
-      const provider = new GoogleAuthProvider()
-      const auth = getAuth();
-      signInWithPopup(auth, provider).then((result) => {
-        // const user = result.user
-        const user = 'hoge'
-        // ↓は正常に動作している
-        this.saveUser(user)
-        console.log(result.user.email)
-      }).catch((error) => {
-        const errorCode = error.code
-        const errorMessage = error.message
-        const email = error.email
-        console.log('errorCode: ' + errorCode + '/errorMessage: ' + errorMessage + '/email: '+ email)
-      })
+      this.$store.dispatch('firebase/login')
     },
   },
 }
