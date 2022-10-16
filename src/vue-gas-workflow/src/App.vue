f38b8adc8dda1ceceebf7f252e07f133ce0804a4
yukimasaki <woodenhoe@gmail.com>
2022-10-16 07:10:27 +0000

    update: App.vue ツールバーにログインユーザーの表示名を表示する処理を追記

--- src/vue-gas-workflow/src/App.vue
+++ src/vue-gas-workflow/src/App.vue
@@ -13,7 +13,7 @@
         <v-btn text to="/">トップ</v-btn>
 
         <!-- ユーザーアイコンのボタン -->
-        <v-btn text to="/login">user_name</v-btn>
+        <v-btn text to="/login">{{ userName }}</v-btn>
 
         <!-- 歯車アイコンのボタン -->
         <v-btn text to="/settings">設定</v-btn>
@@ -37,27 +37,54 @@
 
 <script>
 import { mapState } from 'vuex'
+import firebaseApp from './firebase/firebaseConfig'
 
 export default {
   name: 'App',
 
   data () {
     return {
-      snackbar: false
+      snackbar: false,
     }
   },
 
-  // ミューテーション経由でstateの設定を読み込む
-  // beforeCreate()は［Appインスタンスは生成後 かつ データ初期化前］に実行される
-  // 参考：https://qiita.com/ksh-fthr/items/2a9f173c706ef6939f93
-  beforeCreate () {
-    this.$store.dispatch('workflow/loadSettings')
+  computed: {
+    ...mapState({
+      appName: state => state.workflow.settings.appName,
+      errorMessage: state => state.workflow.errorMessage,
+      userName: state => state.firebase.userName
+    }),
+
   },
 
-  computed: mapState({
-    appName: state => state.workflow.settings.appName,
-    errorMessage: state => state.workflow.errorMessage
-  }),
+  beforeCreate () {
+    // ミューテーション経由でstateの設定を読み込む
+    // beforeCreate()は［Appインスタンスは生成後 かつ データ初期化前］に実行される
+    // 参考：https://qiita.com/ksh-fthr/items/2a9f173c706ef6939f93
+    this.$store.dispatch('workflow/loadSettings')
+
+    // 認証状態を取得
+    this.$store.dispatch('firebase/onAuth')
+  },
+
+  methods: {
+    // Googleアカウントでログイン
+    loginWithGoogle() {
+      // 初期化
+      firebaseApp
+
+      this.$store.dispatch('firebase/login')
+    },
+
+    // ログアウト
+    logout(){
+      // 初期化
+      firebaseApp
+
+      this.$store.dispatch('firebase/logout')
+    }
+  },
+
 
   watch: {
     errorMessage () {
