b6e09fc7b7bb6862cceeecb722c43d898b343aca
yukimasaki <woodenhoe@gmail.com>
2022-10-17 03:40:36 +0000

    update: /loginルーティング追加、ナビゲーションガード仮実装

--- src/vue-gas-workflow/src/router/index.js
+++ src/vue-gas-workflow/src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Settings from '../views/Settings.vue'
 import PaidLeave from '../views/PaidLeave.vue'
 
@@ -11,23 +13,61 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {requiresAuth: true}
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {requiresAuth: false},
+    beforeEnter(to, from, next) {
+      if(store.getters['firebase/getLoginStatus']) {
+        // ログイン時はトップページにリダイレクト
+        next('/')
+      } else {
+        // 未ログイン時はログインページを表示
+        next()
+      }
+    }
   },
   {
     path: '/settings',
     name: 'Settings',
-    component: Settings
+    component: Settings,
+    meta: {requiresAuth: true}
   },
   {
     path: '/paid_leave',
     name: 'PaidLeave',
-    component: PaidLeave
+    component: PaidLeave,
+    meta: {requiresAuth: true}
   },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+// ログイン状態をチェック（Boolean: true/false）
+function isLogin() {
+  return store.getters['firebase/getLoginStatus']
+}
+
+// 未認証の場合はログイン画面('/login')へリダイレクト
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // requiresAuthがtrueなら評価
+    if (!isLogin()) {
+      // 未ログイン時はログインページへリダイレクト
+      next('/login')
+    } else {
+      next()  // スルー
+    }
+  } else {
+    next()  // スルー
+  }
 })
 
 export default router
