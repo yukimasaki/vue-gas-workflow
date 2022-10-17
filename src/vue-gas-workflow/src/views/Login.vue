b01ca802fd7618f46660a101acfa86fe49c20021
yukimasaki <woodenhoe@gmail.com>
2022-10-17 06:45:47 +0000

    update: Login.vue ログインボタンを配置

--- src/vue-gas-workflow/src/views/Login.vue
+++ src/vue-gas-workflow/src/views/Login.vue
@@ -1,6 +1,25 @@
 <template>
-  <p>ログインしてね</p>
+  <v-row justify="center">
+    <v-col cols="12" sm="12" md="4">
+      <v-card
+        height="200px"
+      >
+        <v-card-title class="justify-center" mb-3>
+          <h1 class="display-1">ログイン</h1>
+        </v-card-title>
+
+        <v-card-actions class="justify-center">
+          <v-btn
+            style="text-transform: none"
+            width="80%"
+          >
+          Googleアカウントでログイン</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+
 
 <script>
 </script>
