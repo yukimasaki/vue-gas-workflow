dba74464ce1f963a930ad43aaa2752324d4e5988
yukimasaki <woodenhoe@gmail.com>
2022-10-20 07:58:28 +0000

    update: employeesの全件取得と作成を実装

--- src/vue-gas-workflow/src/store/firestore.js
+++ src/vue-gas-workflow/src/store/firestore.js
@@ -1,23 +1,88 @@
 import db from '@/firebase/firebaseConfig'
 import {
   collection,
-  addDoc
+  addDoc,
+  query,
+  getDocs
 } from 'firebase/firestore'
 
-const state = {}
+const state = {
+  /** ローディング状態 */
+  loading: {
+    fetch: false,
+    add: false,
+    update: false,
+    delete: false
+  },
 
-const mutations = {}
+  /** エラーメッセージ */
+  errorMessage: '',
+
+  /** 従業員のコレクション */
+  employees: []
+}
+
+const mutations = {
+  /** ローディング状態をセットする */
+  setLoading (state, { type, v }) {
+    state.loading[type] = v
+  },
+
+  /** エラーメッセージをセットする */
+  setErrorMessage (state, { message }) {
+    state.errorMessage = message
+  },
+
+  /** 取得したデータをセットする */
+  setAllCollections(state, { collections }) {
+    state.employees = collections
+  },
+
+  /** データを追加する */
+  addCollection(state, { item }) {
+    const list = state.employees
+    if (list) {
+      list.push(item)
+    }
+  },
+}
 
 const actions = {
-  async addDocTest() {
-    const colRef = collection(db, "cities")
-    const data = {
-      name: "Los Angeles",
-      state: "CA",
-      country: "USA"
+  // データを取得する
+  async fetchAllCollections({ commit }) {
+    const type = 'fetch'
+    commit('setLoading', { type, v: true })
+    try {
+      const q = query(collection(db, 'employees'))
+      const querySnapshot = await getDocs(q)
+      const collections = []
+      querySnapshot.forEach(doc => {
+        collections.push(doc.data())
+      })
+
+      commit('setAllCollections', { collections })
+    } catch(e) {
+      commit('setErrorMessage', { message: e })
+      commit('setAllCollections', { collections: [] })
+    } finally {
+      commit('setLoading', { type, v: false})
     }
-    await addDoc(colRef, data)
-  }
+  },
+
+  // データを作成する
+  async addCollection({ commit }, { item }) {
+    const type = 'add'
+    commit('setLoading', { type, v: true })
+    try {
+      const colRef = collection(db, 'employees')
+      await addDoc(colRef, item)
+      commit('addCollection', { item })
+    } catch (e) {
+      commit('setErrorMessage', { message: e })
+    } finally {
+      commit('setLoading', { type, v: false })
+    }
+  },
 }
 
 const getters = {}
