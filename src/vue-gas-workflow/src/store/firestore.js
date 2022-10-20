91d0410fdbbea50035fe6f1ae42e6c86dd2ab980
yukimasaki <woodenhoe@gmail.com>
2022-10-20 01:22:22 +0000

    update: addDocTestをfirestore.jsに移動させた

--- src/vue-gas-workflow/src/store/firestore.js
+++ src/vue-gas-workflow/src/store/firestore.js
@@ -1,10 +1,23 @@
-// import db from '@/firebase/firebaseConfig.js'
+import db from '@/firebase/firebaseConfig'
+import {
+  collection,
+  addDoc
+} from 'firebase/firestore'
 
 const state = {}
 
 const mutations = {}
 
 const actions = {
+  async addDocTest() {
+    const colRef = collection(db, "cities")
+    const data = {
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+    }
+    await addDoc(colRef, data)
+  }
 }
 
 const getters = {}
