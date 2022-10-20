8012f3118d2d39a57049fd817895d81951650cd6
yukimasaki <woodenhoe@gmail.com>
2022-10-20 01:18:56 +0000

    update: FirestoreTest.vue addDocTest実装

--- src/vue-gas-workflow/src/views/FirestoreTest.vue
+++ src/vue-gas-workflow/src/views/FirestoreTest.vue
@@ -1,5 +1,27 @@
 <template>
+  <v-btn @click="this.addDocTest">addDocTest</v-btn>
 </template>
 
 <script>
+import db from '@/firebase/firebaseConfig'
+import {
+  collection,
+  addDoc
+} from 'firebase/firestore'
+
+export default {
+  name: 'FirestoreTest',
+
+  methods: {
+    async addDocTest() {
+      const colRef = collection(db, "cities")
+      const data = {
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA"
+      }
+      await addDoc(colRef, data)
+    }
+  }
+}
 </script>
