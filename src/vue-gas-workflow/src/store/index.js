bf39668e2a9d978fb2a3d39b6d858e4ed18cef4b
yukimasaki <woodenhoe@gmail.com>
2022-10-20 00:56:52 +0000

    firestore.jsをstore/index.jsに追加

--- src/vue-gas-workflow/src/store/index.js
+++ src/vue-gas-workflow/src/store/index.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import workflow from '@/store/workflow.js'
 import firebase from '@/store/firebase.js'
+import firestore from '@/store/firestore.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store ({
-  modules: { workflow, firebase }
+  modules: { workflow, firebase, firestore }
 })
