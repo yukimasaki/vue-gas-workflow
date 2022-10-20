bec39603ab9c0280000082d10bd33743df45681f
yukimasaki <woodenhoe@gmail.com>
2022-10-20 00:41:44 +0000

    add: firestore.jsを作成

---
+++ src/vue-gas-workflow/src/store/firestore.js
@@ -0,0 +1,17 @@
+import {
+  collection,
+  addDoc
+} from 'firebase/firestore'
+
+const state = {}
+const mutations = {}
+const actions = {}
+const getters = {}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
