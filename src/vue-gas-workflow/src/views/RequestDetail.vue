ccdf7b6b64afe377fd72726d5d4597ea99799a8b
yukimasaki <woodenhoe@gmail.com>
2022-11-04 06:39:19 +0000

    add: RequestDetail.vue作成

---
+++ src/vue-gas-workflow/src/views/RequestDetail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {
+  name: 'RequestDetail',
+
+  data() {
+    return {
+    }
+  },
+
+  methods: {
+  },
+}
+</script>
