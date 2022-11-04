0b1a757b2d56b9dd37125f345a9281d7f7b7a947
yukimasaki <woodenhoe@gmail.com>
2022-11-04 07:08:34 +0000

    update: 詳細データを取得・描画する処理を実装

--- src/vue-gas-workflow/src/views/RequestDetail.vue
+++ src/vue-gas-workflow/src/views/RequestDetail.vue
@@ -1,17 +1,50 @@
 <template>
-  <div>{{ $route.params.id }}</div>
+  <div>
+    {{ $route.params.id }}<br>
+    {{ request_details }}
+  </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
+
 export default {
   name: 'RequestDetail',
 
   data() {
     return {
+      currentTableName: 'request_details',
+      data: [],
     }
   },
 
+  computed: {
+    ...mapState({
+      request_details: state => state.firestore.request_details,
+    })
+  },
+
   methods: {
+    ...mapActions({
+      fetchCollectionsByOneQuery: 'firestore/fetchCollectionsByOneQuery'
+    }),
+
+    async fetchRequestDetail() {
+      const currentTableName = this.currentTableName
+      const customQuery = {
+        field: 'snippet_ref',
+        compare: '==',
+        value: this.$route.params.id
+      }
+
+      await this.fetchCollectionsByOneQuery({ currentTableName, customQuery })
+      this.data = this.request_details
+      console.log(this.data)
+    },
+  },
+
+  created() {
+    this.fetchRequestDetail()
   },
 }
 </script>
