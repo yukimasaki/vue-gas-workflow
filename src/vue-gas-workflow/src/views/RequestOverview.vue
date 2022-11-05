d9892241c6139fbe036461177047794be14c567f
yukimasaki <woodenhoe@gmail.com>
2022-11-05 02:24:01 +0000

    update: RequestOverview.vue 末尾セミコロンを削除

--- src/vue-gas-workflow/src/views/RequestOverview.vue
+++ src/vue-gas-workflow/src/views/RequestOverview.vue
@@ -131,13 +131,13 @@ export default {
         if (!format) {
             format = 'YYYY/MM/DD hh:mm'
         }
-        format = format.replace(/YYYY/g, date.getFullYear());
-        format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
-        format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
-        format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
-        format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
-        format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
-        return format;
+        format = format.replace(/YYYY/g, date.getFullYear())
+        format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2))
+        format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2))
+        format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2))
+        format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2))
+        format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2))
+        return format
     },
 
   },
