cf6b968e21f1fb8f06ebc4f18c7de0d035956efe
yukimasaki <woodenhoe@gmail.com>
2022-12-12 04:14:36 +0000

    update: メール送信テスト用のTest.vueを作成

--- src/vue-gas-workflow/src/views/Test.vue
+++ src/vue-gas-workflow/src/views/Test.vue
@@ -3,154 +3,66 @@
     <v-row>
       <v-col col="12">
         <v-card>
-          <v-card-title>users</v-card-title>
+          <v-card-title>{{ title }}</v-card-title>
           <v-card-text>
-            {{ users }}
             <v-spacer></v-spacer>
           </v-card-text>
           <v-card-actions>
-            <v-btn dark color="green" @click="onClickAddUser">
-              <v-icon left>mdi-plus</v-icon>
-              usersを作成
+            <v-btn dark color="green" @click="sendEmail">
+              <v-icon left>mdi-send</v-icon>
+              メール送信
             </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
-
-    <v-row>
-      <v-col col="12">
-        <v-card>
-          <v-card-title>myRequests</v-card-title>
-          <v-card-text>
-            {{ myRequests }}
-            <v-spacer></v-spacer>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn dark color="green" @click="onClickAddRequest">
-              <v-icon left>mdi-plus</v-icon>
-              requestsを作成
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col col="12">
-        <v-card>
-          <v-card-title>othersRequests</v-card-title>
-          <v-card-text>
-            {{ othersRequests }}
-            <v-spacer></v-spacer>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-
-    <ItemDialogUsers ref="ItemDialogUsers"/>
-    <ItemDialogRequests ref="ItemDialogRequests"/>
-
-    <DeleteDialog ref="deleteDialog"/>
 
   </div>
 </template>
 
 <script>
-import ItemDialogUsers from '../components/ItemDialogUsers.vue'
-import ItemDialogRequests from '../components/ItemDialogRequests.vue'
-import DeleteDialog from '../components/DeleteDialog.vue'
-import { mapActions, mapState, mapGetters } from 'vuex'
+import { createTransport } from 'nodemailer'
 
 export default {
   name: 'Test',
 
-  components: {
-    ItemDialogUsers,
-    ItemDialogRequests,
-    DeleteDialog
-  },
-
   data() {
     return {
-      /** 操作対象のテーブル */
-      currentTableName: 'users',
       /** 申請書タイトル */
-      title: '従業員一覧',
-      /** 検索文字 */
-      search: '',
-      /** テーブルに表示させるデータ */
-      tableData: [],
+      title: 'メールテスト',
     }
   },
 
   computed: {
-    ...mapState({
-      users: state => state.firestore.users,
-      myRequests: state => state.firestore.myRequests,
-      othersRequests: state => state.firestore.othersRequests,
-    }),
-
-    /** テーブルのヘッダー設定 */
-    tableHeaders () {
-      return [
-        { text: '部署', value: 'department', sortable: true },
-        { text: 'メールアドレス', value: 'email', sortable: false },
-        { text: '氏名', value: 'name', sortable: false },
-        { text: '操作', value: 'actions', sortable: false },
-      ]
-    },
-
-    /** テーブルのフッター設定 */
-    footerProps () {
-      return { itemsPerPageText: '', itemsPerPageOptions: [30, -1] }
-    },
   },
 
   methods: {
-    ...mapGetters({
-      getUserEmail: 'firebase/getUserEmail',
-    }),
+    async sendEmail() {
+      const transporter = createTransport({
+        host: process.env.EMAIL_HOST,
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_AUTH_USER,
+          pass: process.env.EMAIL_AUTH_PASS
+        }
+      })
 
-    ...mapActions({
-      fetchAllCollections: 'firestore/fetchAllCollections',
-      fetchMyRequests: 'firestore/fetchMyRequests',
-      fetchOthersRequests: 'firestore/fetchOthersRequests',
-    }),
-
-    /** 追加ボタンがクリックされたとき */
-    onClickAddUser () {
-      this.$refs.ItemDialogUsers.open('add')
-    },
-    onClickAddRequest () {
-      this.$refs.ItemDialogRequests.open('add')
-    },
-
-    /** 編集ボタンがクリックされたとき */
-    onClickEdit (item) {
-      this.$refs.ItemDialogUsers.open('edit', item)
-    },
-
-    /** 削除ボタンがクリックされたとき */
-    onClickDelete (item) {
-      const currentTableName = this.currentTableName
-      this.$refs.deleteDialog.open(item, currentTableName)
-    },
-
-    /** テーブルに表示させるデータを取得する */
-    async getRecords() {
-      const currentTableName = this.currentTableName
-      await this.fetchAllCollections({ currentTableName })
-
-      const userId = this.getUserEmail()
-      await this.fetchMyRequests({ userId })
-      await this.fetchOthersRequests({ userId })
-    },
-
+      try {
+        await transporter.sendMail({
+          from: `"ワークフロー" <${process.env.EMAIL_FROM}>`,
+          to: `${process.env.EMAIL_TO}`,
+          subject: `申請がありました。`,
+          text: `テスト送信です。`
+        })
+      } catch (error) {
+        console.log(`メール送信に失敗しました。`)
+        throw error
+      }
+    }
   },
 
   created() {
-    this.getRecords()
   },
 
 }
