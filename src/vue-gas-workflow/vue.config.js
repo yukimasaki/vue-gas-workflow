1388d9a1c5b5a4e60d00d111588a393dca6cfdee
yukimasaki <woodenhoe@gmail.com>
2022-12-13 07:43:58 +0000

    update: publicPathを/vue-gas-workflowへ変更

--- src/vue-gas-workflow/vue.config.js
+++ src/vue-gas-workflow/vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   ],
   outputDir: 'docs',
   assetsDir: './',
-  publicPath: './',
+  publicPath: '/vue-gas-workflow',
   filenameHashing: false,
   productionSourceMap: false
 })
