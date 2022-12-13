f47598f330bf0d2279f7f3ccb6cc1774256baeea
yukimasaki <woodenhoe@gmail.com>
2022-12-13 07:11:45 +0000

    update: vue.config.js修正

--- src/vue-gas-workflow/vue.config.js
+++ src/vue-gas-workflow/vue.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
+  publicPath: isProd ? '/workflow' : '/',
   outputDir: 'docs',
-  publicPath: './'
+  filenameHashing: false,
+  productionSourceMap: false
 })
