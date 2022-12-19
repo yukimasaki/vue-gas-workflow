7e241453a353e7c2fcd011ab47233b511287253c
yukimasaki <woodenhoe@gmail.com>
2022-12-19 02:39:28 +0000

    update: vuetify.js カラーテーマ修正

--- src/vue-gas-workflow/src/plugins/vuetify.js
+++ src/vue-gas-workflow/src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+              primary: '#607d8b',
+              secondary: '#ffc107',
+              accent: '#795548',
+              error: '#f44336',
+              warning: '#ffeb3b',
+              info: '#03a9f4',
+              success: '#8bc34a',
+            },
+        },
+    },
 });
