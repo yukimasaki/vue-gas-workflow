e4378d2e0dddbf8598bd670cb779a5664dbd2359
yukimasaki <woodenhoe@gmail.com>
2023-01-17 08:40:15 +0000

    SendEmailTest.vueを初期状態に戻した

--- src/vue-gas-workflow/src/views/tests/SendEmailTest.vue
+++ src/vue-gas-workflow/src/views/tests/SendEmailTest.vue
@@ -12,7 +12,7 @@
           </v-card-text>
 
           <v-card-actions>
-            <v-btn @click="sendEmail()">test</v-btn>
+            <v-btn>test</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -21,8 +21,6 @@
 </template>
 
 <script>
-const nodemailer = require('nodemailer')
-
 export default {
   name: 'SendEmailTest',
 
@@ -39,37 +37,6 @@ export default {
   },
 
   methods: {
-    async sendEmail() {
-      const transporter = nodemailer.createTransport({
-        host: process.env.VUE_APP_EMAIL_HOST,
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.VUE_APP_EMAIL_USER,
-          pass: process.env.VUE_APP_EMAIL_PASS,
-        },
-      })
-
-      const message = {
-        from: 'process.env.VUE_APP_EMAIL_FROM',
-        to: 'process.env.VUE_APP_EMAIL_TO',
-        subject: 'ワークフローアプリからテスト送信です',
-        text: 'てすとてすと',
-      }
-
-      try {
-        transporter.sendMail(message, function(error, success) {
-          if (error) {
-            console.log('メール送信に失敗しました。')
-            console.log(error.message)
-          }
-          console.log('メール送信に成功しました。')
-          console.log(success.messageId)
-        })
-      } catch (e) {
-        console.log('メール送信時に例外が発生しました。', e)
-      }
-    },
   },
 
 async created() {
