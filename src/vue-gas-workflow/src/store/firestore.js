ef3719478bbef183f50b21adf7948643da4bc564
yukimasaki <woodenhoe@gmail.com>
2023-01-19 05:41:10 +0000

    firestore.jsからgetAdminEmailsアクションを削除

--- src/vue-gas-workflow/src/store/firestore.js
+++ src/vue-gas-workflow/src/store/firestore.js
@@ -343,14 +343,6 @@ const getters = {
   getArrayRoute(state) {
     return state.arrayRoute
   },
-
-  getAdminEmails(state) {
-    const arrayAdminEmail = []
-    state.admins.map(element => {
-      arrayAdminEmail.push(element.id)
-    })
-    return arrayAdminEmail
-  },
 }
 
 export default {
