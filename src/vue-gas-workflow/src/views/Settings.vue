c07e002e1075f5d0caf6c7ce3b7a55a1c9389abb
yukimasaki <woodenhoe@gmail.com>
2023-02-09 08:10:19 +0000

    設定フォームにバリデーションを実装、保存後は保存ボタンを非活性

--- src/vue-gas-workflow/src/views/Settings.vue
+++ src/vue-gas-workflow/src/views/Settings.vue
@@ -11,18 +11,18 @@
               label="API URL"
               v-model="settings.apiUrl"
               :counter="150"
-              :rules="[stringRule]"
+              :rules="apiUrlRules"
             />
             <!-- Auth Token -->
             <v-text-field
               label="Auth Token"
               v-model="settings.authToken"
               :counter="36"
-              :rules="[stringRule]"
+              :rules="authTokenRules"
             />
             <v-row class="mt-4 px-3 pb-3">
               <v-spacer/>
-              <v-btn color="primary" :disabled="!valid" @click="onClickSave">保存</v-btn>
+              <v-btn color="primary" :disabled="(!valid || saveButtonDisabled)" @click="onClickSave">保存</v-btn>
             </v-row>
           </v-form>
         </v-card-text>
@@ -39,18 +39,25 @@ export default {
   data () {
     return {
       settings: { ...this.$store.state.workflow.settings},
-
-      /** 入力したデータが有効かどうか */
       valid: false,
+      saveButtonDisabled: false,
 
       /** バリデーションルール */
-      stringRule: v => v.length <= 150 || '150文字以内で入力してください',
+      apiUrlRules: [
+        v => v.trim().length > 0 || 'API URLは必須です',
+        v => v.trim().length <= 150 || '150文字以内で入力してください',
+      ],
+      authTokenRules: [
+        v => v.trim().length > 0 || 'Auth Tokenは必須です',
+        v => v.trim().length == 36 || '36文字で入力してください',
+      ],
     }
   },
 
   methods: {
     onClickSave () {
       this.$store.dispatch('workflow/saveSettings', { settings: this.settings })
+      this.saveButtonDisabled = !this.saveButtonDisabled
     }
   },
 }
